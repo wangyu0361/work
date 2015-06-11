47d8e6fdcad66f416af12e1bde597713c34cc876
justin <auffant@pacificcontrols.net>
2015-06-11 16:55:35 -0400

    removed unrequired console logs

--- transition-platform/src/main/webapp/intellicommand/icWidgets/eventPage.js
+++ transition-platform/src/main/webapp/intellicommand/icWidgets/eventPage.js
@@ -184,8 +184,6 @@ angular.module('icDash.eventPage', [])
 								 "ticket":entry
 							   } 
 						
-						console.log(timeObj);
-						
 						return timeObj;
 					}
 
@@ -286,14 +284,14 @@ angular.module('icDash.eventPage', [])
 				 )
 				 
 			 }
-			 console.log(currentConfig.stationName, currentConfig.assetName);
+			 
 			 thisController.findAssetByName(currentConfig.stationName,currentConfig.assetName).then( // find all points which belong to an asset
 				function(dbAsset){
 					if(dataStart < +(new Date(currentConfig.chartStart))){dataStart = +(new Date(currentConfig.chartStart))}
 					if(dataEnd < +(new Date(currentConfig.chartEnd))){dataEnd = +(new Date(currentConfig.chartEnd))}
 					$scope.config.allPoints = dbAsset.points.slice(0);
 					currentConfig.allPoints = dbAsset.points.slice(0);
-					console.log(currentConfig);
+					
 					for(var i = 0; i < dbAsset.points.length; i++){
 						addAPIPointsToArray(dbAsset.points[i],dbAsset.points.length);
 					}
